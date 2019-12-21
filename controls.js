6bc6e83c20023c0c0817ad9654c1272ad7d20602
wisesm0 <dyadicofficial@gmail.com>
2019-12-20 23:29:22 -0800

    Add some new scenes

--- controls.js
+++ controls.js
@@ -2,8 +2,25 @@ const playbutton = document.getElementById('play-pause-button');
 const iframage = document.getElementById('ifr');
 const i = playbutton.querySelector('i');
 
+const playnext = document.getElementById('play-next');
+const playprevious = document.getElementById('play-previous');
+
+const scenes = [
+    '../scenes/left-behind/index.html',
+    '../scenes/urban-decay/index.html',
+    '../scenes/big-bang/index.html'
+];
+
 playbutton.addEventListener('click', function() {
     playPause();
+});
+
+playnext.addEventListener('click', () => {
+    playNext();
+});
+
+playprevious.addEventListener('click', () => {
+    playPrevious();
 });
 
 let paused = true;
@@ -26,4 +43,22 @@ function playPause()
             paused = true;
         }
     },200);
+}
+
+let idx = 0;
+
+function playNext() {
+    idx++;
+    if (idx > scenes.length-1) {
+        idx = 0;
+    }
+    iframage.src = scenes[idx];
+}
+
+function playPrevious() {
+    idx--;
+    if (idx < 0) {
+        idx = scenes.length-1;
+    }
+    iframage.src = scenes[idx];
 }
